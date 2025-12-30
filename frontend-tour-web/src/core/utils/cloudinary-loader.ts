9efdd63cddd4cf1d37ae712a71a753ab16cc3d0a
akared45 <vinhle5620@gmail.com>
2025-12-30 17:36:42 +0700

    a

--- frontend-tour-web/src/core/utils/cloudinary-loader.ts
+++ frontend-tour-web/src/core/utils/cloudinary-loader.ts
@@ -1,4 +1,6 @@
 export default function cloudinaryLoader({ src, width, quality }: { src: string, width: number, quality?: number }) {
+    const cloudName = 'dcfaz2rme';
     const params = ['f_auto', 'q_auto', `w_${width}`];
-    return src.replace('/upload/', `/upload/${params.join(',')}/`);
+
+    return `https://res.cloudinary.com/${cloudName}/image/upload/${params.join(',')}/${src}`;
 }
